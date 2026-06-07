27a11c312bfc04e4339174867c50beee2162e9fd
Hamzah "Opal" Nutt <hamzah.nutt@my.jcu.edu.au>
2026-06-07 18:40:46 +0800

    Add json parsing capabilities to LevelData class

--- modules/level_data.py
+++ modules/level_data.py
@@ -2,15 +2,19 @@ from modules.event import Event
 
 class LevelData:
     """Level Data"""
-    def __init__(self, input_data):
+    def __init__(self, input_format, input_data):
+        self.events = []
         self._length = len(input_data)
-        if self.is_valid():
-            self.events = []
-            for offset in range(0, len(input_data), 40):
-                self.events.append(Event.from_bytes(input_data[offset:offset + 40]))
+        if input_format == "bytes":
+            if self.is_valid_bytes():
+                for offset in range(0, len(input_data), 40):
+                    self.events.append(Event.from_bytes(input_data[offset:offset + 40]))
+        elif input_format == "JSON":
+            for item in input_data:
+                self.events.append(Event.from_json(item))
 
     def __str__(self):
-        if self.is_valid():
+        if self.is_valid_bytes():
             return "Platypus II level data"
         else:
             return "Invalid level data file"
@@ -18,7 +22,15 @@ class LevelData:
     def __repr__(self):
         self.__str__()
 
-    def is_valid(self):
+    @classmethod
+    def from_bytes(cls, input_bytes):
+        return cls("bytes", input_bytes)
+
+    @classmethod
+    def from_json(cls, input_json):
+        return cls("JSON", input_json)
+
+    def is_valid_bytes(self):
         return self._length % 4 == 0
 
     def to_json(self):
@@ -26,3 +38,9 @@ class LevelData:
         for event in self.events:
             output_json.append(event.to_json())
         return output_json
+
+    def to_bytes(self):
+        output_string = b""
+        for event in self.events:
+            output_string += event.to_bytes()
+        return output_string
